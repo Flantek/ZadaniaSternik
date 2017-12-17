9f541fa026641558c201c89a49491c5e4461d73a
Flantek <mateusz.jacykowski@edu.uekat.pl>
2017-12-17 10:21:20 +0100

    Zadania

---
+++ zad-01/pl/sternik/mj/zadania/Zad01.java
@@ -0,0 +1,7 @@
+package pl.sternik.mj.zadania;
+
+public class Zad01{
+    public static void main(String[] args) {
+        System.out.println("Witaj"+ "\n"+ "Sternik");
+    }
+}
---
+++ zad-03/src/pl/sternik/mj/zadania/Zad03.java
@@ -0,0 +1,12 @@
+package pl.sternik.mj.zadania;
+
+public class Zad03 {
+    public static void main(String[] args) {
+        int i = 0;
+        i++;
+        i--;
+        System.out.println("Elo");
+        System.out.println("Siema");
+        System.out.println("5");
+    }
+}
